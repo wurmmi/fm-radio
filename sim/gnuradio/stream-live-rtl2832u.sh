425590cb5fe2c0da0d6aca5391eb4ccb86c00dba
Michael Wurm <wurm.michael95@gmail.com>
2020-10-20 22:34:39 +0200

    add recording script

--- sim/gnuradio/stream-live-rtl2832u.sh
+++ sim/gnuradio/stream-live-rtl2832u.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+################################################################################
+# File        : stream-live-rtl2832u.sh
+# Author      : Michael Wurm <wurm.michael95@gmail.com>
+# Description : Streams a FM radio station from RTL2832u.
+################################################################################
+
 rtl_fm -f 88.8e6 -s 200000 -r 48000 | aplay -r 48000 -f S16_LE
