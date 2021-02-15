a2fb6103b167b37cf30c9b5e5c689549de6e2318
Michael Wurm <wurm.michael95@gmail.com>
2021-02-15 21:00:50 +0100

    build spectrum

--- sim/matlab/fm_transceiver.m
+++ sim/matlab/fm_transceiver.m
@@ -12,40 +12,44 @@ addpath(genpath('./helpers/auto-arrange-figs/'));
 
 %% Settings
 % Channel
-fc_oe3 = 98.1e6;
+fc_oe3_Hz = 98.1e6;
 
-fs    = 1e6;
-n_sec = 1;
-
-% Options
+% Simulation Options
 EnableTrafficInfoTrigger = false;
+EnableAudioReplay        = false;
+
+n_sec = 2;
+fs_Hz = 500e3;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Sender
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Audio stream data
-t = 0:1/fs:n_sec-1;
+% TODO: read this from a file
+tn = 0:1:n_sec*fs_Hz-1;
 
-audioFreqLHz = 500;
-audioDataL   = 1 * sin(2*pi*audioFreqLHz/fs*t);
+audioFreqL_Hz = 400;
+audioDataL   = 1 * sin(2*pi*audioFreqL_Hz/fs_Hz*tn);
 
-audioFreqRHz = 500;
-audioDataR   = 1 * sin(2*pi*audioFreqRHz/fs*t);
+audioFreqR_Hz = 500;
+audioDataR   = 1 * sin(2*pi*audioFreqR_Hz/fs_Hz*tn);
 
 audioData = audioDataL + audioDataR;
+% TODO: upsample (44.1k -> fs), then used upsampled freq (fs) for following tones
 
 %% 19kHz pilot tone
 
-pilotFreqHz = 19000;
-pilotTone = 1 * sin(2*pi*pilotFreqHz/fs);
+pilotFreq_Hz = 19000;
+pilotTone = 0.25 * sin(2*pi*pilotFreq_Hz/fs_Hz*tn);
 
 %% Difference signal (for stereo)
 
 audioDiff = audioDataL - audioDataR;
 
 % Modulate it to 38 kHz
-audioDiffMod = audioDiff * 0.5; %TODO
+carrier4Diff = 1 * sin(2*pi*38e3/fs_Hz*tn);
+audioDiffMod = audioDiff .* carrier4Diff;
 
 %% Radio Data Signal (RDS)
 % TODO
@@ -65,15 +69,30 @@ fmChannel = audioData + pilotTone + audioDiffMod + hinzTriller;
 
 %% FM channel spectrum
 
+% Downsample
+NDown = 1;
+fmChannelDown = downsample(fmChannel,NDown);
+
 % FFT
+Nfft = 4096*2;
+fmChannelSpec = ( abs( fftshift( fft(fmChannelDown,Nfft) )));
+
+fft_freqs = (-Nfft/2:1:Nfft/2-1)*fs_Hz/NDown/Nfft;
 
 % Plot
-figure();
-
-
-
-
+figure('Name','FM channel spectrum (linear)');
+plot(fft_freqs, fmChannelSpec);
+grid on;
+title('FM channel spectrum (linear)');
+xlabel('Hz')
+ylabel('magnitude')
+xlim([0 65e3])
+xline(19e3,'r--','19 kHz');
+xline(38e3,'r--','38 kHz');
+xline(57e3,'r--','57 kHz');
 
 
 %% Arrange all plots on the display
-autoArrangeFigures(4,4,1);
+autoArrangeFigures(2,2,1);
+
+
