6a3af6d63414a9f39a1c8e68b5217190819ed114
Michael Wurm <wurm.michael95@gmail.com>
2021-02-16 15:19:56 +0100

    wip; fm sender

--- sim/matlab/fm_transceiver.m
+++ sim/matlab/fm_transceiver.m
@@ -6,20 +6,23 @@
 
 %% Prepare environment
 clear; close all; clc;
-%restoredefaultpath;
 
 addpath(genpath('./helpers/auto-arrange-figs/'));
 
 %% Settings
-% Channel
-fc_oe3_Hz = 98.1e6;
 
 % Simulation Options
 EnableTrafficInfoTrigger = false;
 EnableAudioReplay        = false;
+EnableAudioFromFile      = false;
 
-n_sec = 2;
-fs_Hz = 500e3;
+% Signal parameters
+n_sec = 0.1;
+osr   = 20;
+fs_Hz = 44.1e3 * osr;
+
+% Channel
+fc_oe3_Hz = 98.1e6;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Sender
@@ -29,14 +32,29 @@ fs_Hz = 500e3;
 % TODO: read this from a file
 tn = 0:1:n_sec*fs_Hz-1;
 
-audioFreqL_Hz = 400;
-audioDataL   = 1 * sin(2*pi*audioFreqL_Hz/fs_Hz*tn);
+if EnableAudioFromFile
+    fs_file = 44.1e3;
+    samples = [1, n_sec*fs_file];
+    [fileData,Fs] = audioread('./recordings/left-right-test.mp3', samples);
+    if Fs ~= fs_file
+        error("Unexpected sample frequency of file!");
+    end
+    audioDataL = fileData(:,1);
+    audioDataR = fileData(:,2);
+    
+    audioData = audioDataL + audioDataR;
+else
+    audioFreqL_Hz = 400;
+    audioDataL    = 1 * sin(2*pi*audioFreqL_Hz/fs_Hz*tn)';
+    
+    audioFreqR_Hz = 500;
+    audioDataR    = 1 * sin(2*pi*audioFreqR_Hz/fs_Hz*tn)';
+    
+    audioData = audioDataL + audioDataR;
+end
 
-audioFreqR_Hz = 500;
-audioDataR   = 1 * sin(2*pi*audioFreqR_Hz/fs_Hz*tn);
-
-audioData = audioDataL + audioDataR;
-% TODO: upsample (44.1k -> fs), then used upsampled freq (fs) for following tones
+% Upsample
+audioData = resample(audioData, osr, 1);
 
 %% 19kHz pilot tone
 
@@ -49,7 +67,7 @@ audioDiff = audioDataL - audioDataR;
 
 % Modulate it to 38 kHz
 carrier4Diff = 1 * sin(2*pi*38e3/fs_Hz*tn);
-audioDiffMod = audioDiff .* carrier4Diff;
+audioLRDiffMod = audioDiff .* carrier4Diff;
 
 %% Radio Data Signal (RDS)
 % TODO
@@ -65,31 +83,61 @@ end
 %% FM channel
 % Sum up all signal parts
 
-fmChannel = audioData + pilotTone + audioDiffMod + hinzTriller;
+tx_fmChannel = audioData + pilotTone + audioLRDiffMod + hinzTriller;
 
 %% FM channel spectrum
 
 % Downsample
 NDown = 1;
-fmChannelDown = downsample(fmChannel,NDown);
+tx_fmChannelDown = downsample(tx_fmChannel,NDown);
 
 % FFT
 Nfft = 4096*2;
-fmChannelSpec = ( abs( fftshift( fft(fmChannelDown,Nfft) )));
+fmChannelSpec = ( abs( fftshift( fft(tx_fmChannelDown,Nfft) )));
 
 fft_freqs = (-Nfft/2:1:Nfft/2-1)*fs_Hz/NDown/Nfft;
 
-% Plot
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Receiver
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Analysis
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Create output folder to save figures
+outputDir = "./matlab_output/";
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir)
+end
+
+figure('Name','TX Time domain signal');
+grid on; hold on;
+plot(tn/fs_Hz, audioData,     'r', 'DisplayName', 'audioData');
+plot(tn/fs_Hz, pilotTone,     'm', 'DisplayName', 'pilotTone');
+plot(tn/fs_Hz, audioLRDiffMod,'k', 'DisplayName', 'audioLRDiffMod');
+plot(tn/fs_Hz, tx_fmChannel,  'b','DisplayName', 'Total');
+title('Time domain signal');
+xlabel('time [s]');
+ylabel('amplitude');
+legend();
+xlim([0 inf]);
+saveas(gcf, outputDir + "tx_time_domain.png");
+
 figure('Name','FM channel spectrum (linear)');
-plot(fft_freqs, fmChannelSpec);
-grid on;
-title('FM channel spectrum (linear)');
-xlabel('Hz')
-ylabel('magnitude')
-xlim([0 65e3])
+grid on; hold on;
 xline(19e3,'r--','19 kHz');
 xline(38e3,'r--','38 kHz');
 xline(57e3,'r--','57 kHz');
+plot(fft_freqs, fmChannelSpec);
+title('FM channel spectrum (linear)');
+xlabel('frequency [Hz]');
+ylabel('magnitude');
+xlim([0 65e3]);
+saveas(gcf, outputDir + "tx_freq_domain.png");
 
 
 %% Arrange all plots on the display
