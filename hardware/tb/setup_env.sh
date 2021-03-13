7f07812fe1fae28afdbb4f51338846fe3bdb958c
Michael Wurm <wurm.michael95@gmail.com>
2021-03-13 13:22:24 +0100

    header comment

--- hardware/tb/setup_env.sh
+++ hardware/tb/setup_env.sh
@@ -1,7 +1,13 @@
+################################################################################
+# File        : setup_env.sh
+# Author      : Stefan Jahn <stefan.jahn332@gmail.com>
+#               Michael Wurm <wurm.michael95@gmail.com>
+# Description : Set up environment for cocotb testbench.
+################################################################################
 
 PROJECT_ROOT="$( cd "$(dirname "$BASH_SOURCE")/../.." >/dev/null 2>&1 ; pwd -P )"
 
-if [ $BASH_SOURCE == $0 ];then
+if [ $BASH_SOURCE == $0 ]; then
     echo "Failure, this script must be called with source"
     echo "e.g. \"source setup_env.sh\""
     exit 1
