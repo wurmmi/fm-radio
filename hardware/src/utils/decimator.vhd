5b61e0f315b8fa921d261541c16094c535f135f5
Michael Wurm <wurm.michael95@gmail.com>
2021-03-15 13:27:43 +0100

    impl decimator

--- hardware/src/utils/decimator.vhd
+++ hardware/src/utils/decimator.vhd
@@ -8,18 +8,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.fixed_pkg.all;
 
 library work;
 use work.fm_pkg.all;
 
 entity decimator is
+  generic (
+    decimation_g : natural := 1);
   port (
     clk_i : in std_ulogic;
     rst_i : in std_ulogic;
 
-    sample_i       : in  sample_t;
-    sample_valid_i : in  std_ulogic;
+    sample_i       : in sample_t;
+    sample_valid_i : in std_ulogic;
 
     sample_o       : out sample_t;
     sample_valid_o : out std_ulogic);
@@ -29,45 +30,57 @@ end entity decimator;
 architecture rtl of decimator is
 
   -----------------------------------------------------------------------------
-  --! @name Types and Constants
-  -----------------------------------------------------------------------------
-  --! @{
-
-  --! @}
-  -----------------------------------------------------------------------------
   --! @name Internal Registers
   -----------------------------------------------------------------------------
   --! @{
 
+  signal count : natural range 0 to decimation_g - 1 := 0;
 
-  --! @}
-  -----------------------------------------------------------------------------
-  --! @name Internal Wires
-  -----------------------------------------------------------------------------
-  --! @{
-
+  signal sample : sample_t   := (others => '0');
+  signal valid  : std_ulogic := '0';
 
   --! @}
 
-begin  -- architecture rtl
+begin -- architecture rtl
 
-  ------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
   -- Outputs
-  ------------------------------------------------------------------------------
-
-  -----------------------------------------------------------------------------
-  -- Signal Assignments
   -----------------------------------------------------------------------------
 
+  sample_valid_o <= valid;
+  sample_o       <= sample;
 
-  ------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
   -- Registers
-  ------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
 
-  ------------------------------------------------------------------------------
-  -- Instantiations
-  ------------------------------------------------------------------------------
+  regs : process (clk_i) is
+    procedure reset is
+    begin
+      valid  <= '0';
+      count  <= 0;
+      sample <= (others => '0');
+    end procedure;
+  begin -- process regs
+    if rising_edge(clk_i) then
+      if rst_i = '1' then
+        reset;
+      else
+        -- Defaults
+        valid <= '0';
 
-  -- LP Filter (?)
+        if sample_valid_i = '1' then
+          if count = decimation_g - 1 then
+            count <= 0;
+
+            valid  <= '1';
+            sample <= sample_i;
+          else
+            count <= count + 1;
+          end if;
+        end if;
+      end if;
+    end if;
+  end process regs;
 
 end architecture rtl;
