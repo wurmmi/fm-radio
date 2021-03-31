1090f0a5f1c3a1cc9204b253ae40435d2f4ec647
Michael Wurm <wurm.michael95@gmail.com>
2021-03-31 14:50:48 +0200

    add strobe_gen

---
+++ hardware/hls/src/utils/strobe_gen.cpp
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/**
+ * @file    strobe_gen.cpp
+ * @author  Michael Wurm <wurm.michael95@gmail.com>
+ * @brief   Strobe Generator IP implementation.
+ */
+/*****************************************************************************/
+
+#include "strobe_gen.hpp"
+
+using namespace std;
+
+bool strobe_gen() {
+  static uint8_t count = 0;
+
+  if (count >= num_wait_cycles_c - 1) {
+    count = 0;
+    return true;
+  } else {
+    count++;
+  }
+  return false;
+}
