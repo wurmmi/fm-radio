8d3747fafe1b9bdb66e41c4c29b26c0cf700e91e
Michael Wurm <wurm.michael95@gmail.com>
2021-04-11 16:25:36 +0200

    create vivado project

--- hardware/vivado/scripts/create_project.tcl
+++ hardware/vivado/scripts/create_project.tcl
@@ -22,22 +22,28 @@ if {$open_gui == 1} {
   start_gui
 }
 
-puts "--- - Creating new project"
-create_project $proj_name $build_dir -part $device -force
+if {[file exists $build_dir/$proj_name.xpr]} {
+  puts "--- - Opening existing project"
+  open_project $build_dir/$proj_name.xpr
+} elseif {[catch {
+  puts "--- - Creating new project"
+  create_project $proj_name $build_dir -part $device -force
 
-set_property target_language VHDL [current_project]
-set_property ip_repo_paths $ip_dir [current_project]
-update_ip_catalog
+  set_property target_language VHDL [current_project]
+  set_property ip_repo_paths $ip_dir [current_project]
+  update_ip_catalog
 
-set_property BOARD_PART em.avnet.com:zed:part0:1.4 [current_project]
+  set_property BOARD_PART em.avnet.com:zed:part0:1.4 [current_project]
 
-puts "--- - Creating block design"
-source ./bd/$proj_name.tcl
+  puts "--- - Creating block design"
+  source ./bd/$proj_name.tcl
 
-# Generate the wrapper
-set design_name [get_bd_designs]
-make_wrapper -files [get_files $design_name.bd] -top -import
+  # Generate the wrapper
+  set design_name [get_bd_designs]
+  make_wrapper -files [get_files $design_name.bd] -top -import
 
-puts "### Block design created."
-
-exitif 0
+  puts "### Block design created."
+} ]} {
+  puts "ERROR: Project creation failed."
+  exitif 1
+}
