c6b401e83349573ea2f6eca57f1c209abf7f434f
Michael Wurm <wurm.michael95@gmail.com>
2021-05-20 21:14:04 +0200

    remove DELAY testing file writers

--- hardware/hls/src/utils/fm_demodulator.cpp
+++ hardware/hls/src/utils/fm_demodulator.cpp
@@ -17,19 +17,9 @@ using namespace std;
 
 static DELAY<iq_sample_t, 3> delay_inst;
 
-static DataWriter writer_fm_demod_test_in("fm_demod_test_in.txt");
-static DataWriter writer_fm_demod_test_in_del("fm_demod_test_in_del.txt");
-static DataWriter writer_fm_demod_test_out("fm_demod_test_out.txt");
-
 sample_t fm_demodulator(iq_sample_t const& iq) {
-  writer_fm_demod_test_in.write(iq.i);
-  writer_fm_demod_test_in.write(iq.q);
-
   // Delay
   iq_sample_t sample_del = delay_inst(iq);
-
-  writer_fm_demod_test_in_del.write(sample_del.i);
-  writer_fm_demod_test_in_del.write(sample_del.q);
 
   // Differentiate
   sample_t i_sample_diff = iq.i - sample_del.i;
@@ -40,8 +30,6 @@ sample_t fm_demodulator(iq_sample_t const& iq) {
   sample_t demod_part_b = iq.q * i_sample_diff;
 
   sample_t fm_demod = demod_part_a - demod_part_b;
-
-  writer_fm_demod_test_out.write(fm_demod);
 
   return fm_demod;
 }
