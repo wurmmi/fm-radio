7cdd1c177060ff49fa72df4270e66f832c86b724
Michael Wurm <wurm.michael95@gmail.com>
2021-05-21 19:24:19 +0200

    implement looping task - works

--- hardware/vivado/sdk/fm_radio_app/src/main.cpp
+++ hardware/vivado/sdk/fm_radio_app/src/main.cpp
@@ -12,16 +12,27 @@ using namespace std;
 
 const TickType_t delay_ms_c = pdMS_TO_TICKS(1000);
 
-int main() {
-  cout << "cout Hello World!" << endl;
-  printf("printf Hello World!");
-  xil_printf("xil_printf Hello World!");
+static TaskHandle_t task_1;
 
-  uint32_t count = 0;
+static void task_loop(void *) {
+  static uint32_t count = 0;
   while (true) {
     cout << "[" << count++ << "] looping ..." << endl;
     vTaskDelay(delay_ms_c);
   }
+}
 
+int main() {
+  cout << "cout Hello World!" << endl;
+
+  xTaskCreate(task_loop, /* The function that implements the task. */
+              (const char *)"task_loop", /* Text name for the task, provided to
+                                     assist debugging only. */
+              configMINIMAL_STACK_SIZE,  /* The stack allocated to the task. */
+              NULL, /* The task parameter is not used, so set to NULL. */
+              tskIDLE_PRIORITY, /* The task runs at the idle priority. */
+              &task_1);
+
+  vTaskStartScheduler();
   return 0;
 }
