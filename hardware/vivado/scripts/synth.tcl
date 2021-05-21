71b83106ed0f2760857bade2433e380f874ccbeb
Michael Wurm <wurm.michael95@gmail.com>
2021-05-21 13:47:19 +0200

    create hierarchical report

--- hardware/vivado/scripts/synth.tcl
+++ hardware/vivado/scripts/synth.tcl
@@ -9,7 +9,11 @@ set build_dir [lindex $argv 0]
 set proj_name "proj"
 set parallel_jobs 4
 
-if {0} {
+# Create build result folder
+set build_finish_time [clock format [clock seconds] -format %Y%m%d_%H%M%S]
+set result_dir $build_dir/reports_$build_finish_time
+file mkdir $result_dir
+
 # Open project
 if {[catch {
   open_project $build_dir/$proj_name.xpr
@@ -67,6 +71,11 @@ if {[catch {
   exit 1
 }
 
+puts "--- - Creating reports"
+open_run impl_1
+
+report_utilization -hierarchical -hierarchical_percentages -file $result_dir/report_utilization.rpt
+
 if {[catch {
   puts "--- - Creating bitstream"
   reset_run impl_1 -from_step write_bitstream
@@ -76,16 +85,10 @@ if {[catch {
   puts "(MWURM) ERROR: Bitstream generation failed."
   exit 1
 }
-}
 
 puts "--- - Copying build results"
 
-# Create output folder
-set build_finish_time [clock format [clock seconds] -format %y%m%d_%H%M%S]
-set result_dir $build_dir/reports_$build_finish_time
-file mkdir $result_dir
-
-# Reports
+puts "Reports ..."
 set build_output_dir $build_dir/$proj_name.runs/impl_1
 set report_files [ list                                             \
   "$build_output_dir/${proj_name}_wrapper_utilization_placed.rpt"   \
@@ -93,7 +96,7 @@ set report_files [ list                                             \
 
 file copy $report_files $result_dir
 
-# Binaries
+puts "Binaries ..."
 file copy $build_output_dir/${proj_name}_wrapper.bit $result_dir
 file copy $build_output_dir/${proj_name}_wrapper.hwdef $result_dir
 
