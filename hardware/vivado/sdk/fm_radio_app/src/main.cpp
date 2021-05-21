ba4c536bbe33d77b2411d78c692d51a04f769218
Michael Wurm <wurm.michael95@gmail.com>
2021-05-21 18:03:46 +0200

    hello world SDK

--- hardware/vivado/sdk/fm_radio_app/src/main.cpp
+++ hardware/vivado/sdk/fm_radio_app/src/main.cpp
@@ -2,7 +2,26 @@
  * Empty C++ Application
  */
 
-int main()
-{
-	return 0;
+#include <FreeRTOS.h>
+#include <task.h>
+#include <xil_printf.h>
+
+#include <iostream>
+
+using namespace std;
+
+const TickType_t delay_ms_c = pdMS_TO_TICKS(1000);
+
+int main() {
+  cout << "cout Hello World!" << endl;
+  printf("printf Hello World!");
+  xil_printf("xil_printf Hello World!");
+
+  uint32_t count = 0;
+  while (true) {
+    cout << "[" << count++ << "] looping ..." << endl;
+    vTaskDelay(delay_ms_c);
+  }
+
+  return 0;
 }
