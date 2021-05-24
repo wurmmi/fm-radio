13eaf6bc1922193b8a7c3bbcee00a66a124a84b6
Michael Wurm <wurm.michael95@gmail.com>
2021-05-24 16:16:45 +0200

    remove while-loop

--- hardware/vivado/sdk/fm_radio_app/src/AudioStreamFIFO.cpp
+++ hardware/vivado/sdk/fm_radio_app/src/AudioStreamFIFO.cpp
@@ -11,10 +11,10 @@ AudioStreamFIFO::AudioStreamFIFO(uint32_t device_id) : FIFO(device_id) {}
 AudioStreamFIFO::~AudioStreamFIFO() {}
 
 void AudioStreamFIFO::write(audio_sample_t const& sample) {
-  while (!XLlFifo_iTxVacancy(&mDev)) {
-    // Don't do this in an interrupt routine...
-    // printf("I2S FIFO full. Waiting ... \n");
-  }
+  // while (!XLlFifo_iTxVacancy(&mDev)) {
+  // Don't do this in an interrupt routine...
+  // printf("I2S FIFO full. Waiting ... \n");
+  //}
 
   XLlFifo_TxPutWord(&mDev, ((u32)sample.left << 16) | (u32)sample.right);
   XLlFifo_iTxSetLen(&mDev, 1 * FIFO_WORDSIZE);
