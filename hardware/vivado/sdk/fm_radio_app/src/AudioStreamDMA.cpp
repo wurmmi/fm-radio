edb77a4fd1e83b39fb81137e892bae5c0b67ef50
Michael Wurm <wurm.michael95@gmail.com>
2021-05-27 20:58:27 +0200

    dma interrupt

--- hardware/vivado/sdk/fm_radio_app/src/AudioStreamDMA.cpp
+++ hardware/vivado/sdk/fm_radio_app/src/AudioStreamDMA.cpp
@@ -16,6 +16,185 @@ AudioStreamDMA::AudioStreamDMA(uint32_t device_id) : mDeviceId(device_id) {}
 
 AudioStreamDMA::~AudioStreamDMA() {}
 
+void AudioStreamDMA::TxDoneCallback() {
+  cout << "################## DMA Tx DONE" << endl;
+}
+
+bool AudioStreamDMA::TxSetup() {
+  XAxiDma_BdRing* txRingPtr = XAxiDma_GetTxRing(&mDev);
+
+  // Disable all TX interrupts before TxBD space setup
+  XAxiDma_BdRingIntDisable(txRingPtr, XAXIDMA_IRQ_ALL_MASK);
+
+  // Setup TxBD space
+  uint32_t bd_count = XAxiDma_BdRingCntCalc(XAXIDMA_BD_MINIMUM_ALIGNMENT,
+                                            (uint32_t)sizeof(mBdBuffer));
+  cout << "bd_count = " << bd_count << endl;
+  if (bd_count != DMA_BD_BUFFER_SIZE) {
+    LOG_ERROR("something went wrong here - sizes don't match");
+    return false;
+  }
+
+  int status = XAxiDma_BdRingCreate(txRingPtr,
+                                    (UINTPTR)&mBdBuffer[0],
+                                    (UINTPTR)&mBdBuffer[0],
+                                    XAXIDMA_BD_MINIMUM_ALIGNMENT,
+                                    bd_count);
+  if (status != XST_SUCCESS) {
+    printf("Failed create BD ring\n");
+    return false;
+  }
+
+  /**
+   *  Create a template and set all BDs to be the same as the
+   *  template. The sender has to set up the BDs as needed.
+   */
+  XAxiDma_Bd BdTemplate;
+  XAxiDma_BdClear(&BdTemplate);
+  status = XAxiDma_BdRingClone(txRingPtr, &BdTemplate);
+  if (status != XST_SUCCESS) {
+    printf("Failed to clone BDs\n");
+    return false;
+  }
+
+  /*
+   * Set the coalescing threshold.
+
+   * We set the coalescing threshold to be the total number of packets.
+   * The receive side will only get one completion interrupt per cyclic
+   * transfer.
+   *
+   * If you would like to have multiple interrupts to happen, change
+   * the DMA_COALESCING_COUNT to be a smaller value.
+   */
+  status = XAxiDma_BdRingSetCoalesce(
+      txRingPtr, DMA_COALESCING_COUNT, DMA_DELAY_TIMER_COUNT);
+  if (status != XST_SUCCESS) {
+    xil_printf("Failed set coalescing %d/%d\n",
+               DMA_COALESCING_COUNT,
+               DMA_DELAY_TIMER_COUNT);
+    return XST_FAILURE;
+  }
+
+  /* Enable Cyclic DMA mode */
+  XAxiDma_BdRingEnableCyclicDMA(txRingPtr);
+  XAxiDma_SelectCyclicMode(&mDev, XAXIDMA_DMA_TO_DEVICE, 1);
+
+  /* Enable all TX interrupts */
+  XAxiDma_BdRingIntEnable(txRingPtr, XAXIDMA_IRQ_ALL_MASK);
+
+  /* Start the TX channel */
+  status = XAxiDma_BdRingStart(txRingPtr);
+  if (status != XST_SUCCESS) {
+    printf("Failed bd start\n");
+  }
+
+  return true;
+}
+
+/** This is the DMA TX Interrupt handler function.
+ *
+ * It gets the interrupt status from the hardware, acknowledges it,
+ * and if any error happens, it resets the hardware. Otherwise, if a
+ * completion interrupt presents, then it calls the callback function.
+ */
+void AudioStreamDMA::TxIRQHandler() {
+  XAxiDma_BdRing* txRingPtr = XAxiDma_GetTxRing(&mDev);
+
+  /* Read pending interrupts */
+  uint32_t IrqStatus = XAxiDma_BdRingGetIrq(txRingPtr);
+
+  /* Acknowledge pending interrupts */
+  XAxiDma_BdRingAckIrq(txRingPtr, IrqStatus);
+
+  /* If no interrupt is asserted, we do not do anything
+   */
+  if (!(IrqStatus & XAXIDMA_IRQ_ALL_MASK)) {
+    return;
+  }
+
+  /*
+   * If error interrupt is asserted, raise error flag, reset the
+   * hardware to recover from the error, and return with no further
+   * processing.
+   */
+  if ((IrqStatus & XAXIDMA_IRQ_ERROR_MASK)) {
+    XAxiDma_BdRingDumpRegs(txRingPtr);
+
+    mErrorState = 1;
+
+    int timeOut = DMA_RESET_TIMEOUT_COUNTER;
+    XAxiDma_Reset(&mDev);
+    while (timeOut) {
+      if (XAxiDma_ResetIsDone(&mDev)) {
+        break;
+      }
+      timeOut--;
+    }
+    return;
+  }
+
+  /*
+   * If Transmit done interrupt is asserted, call TX call back function
+   * to handle the processed BDs and raise the according flag
+   */
+  if ((IrqStatus & (XAXIDMA_IRQ_DELAY_MASK | XAXIDMA_IRQ_IOC_MASK))) {
+    TxDoneCallback();
+  }
+}
+
+void AudioStreamDMA::TxIRQCallback(void* context) {
+  static_cast<AudioStreamDMA*>(context)->TxIRQHandler();
+}
+
+bool AudioStreamDMA::InterruptSetup() {
+  XScuGic_Config* IntcConfig;
+
+  /*
+   * Initialize the interrupt controller driver so that it is ready to
+   * use.
+   */
+  IntcConfig = XScuGic_LookupConfig(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+  if (IntcConfig == nullptr) {
+    LOG_ERROR("failed XScuGic_LookupConfig()");
+    return XST_FAILURE;
+  }
+
+  int status =
+      XScuGic_CfgInitialize(&mIntCtrl, IntcConfig, IntcConfig->CpuBaseAddress);
+  if (status != XST_SUCCESS) {
+    return XST_FAILURE;
+  }
+
+  XScuGic_SetPriorityTriggerType(&mIntCtrl, mIntCtrlId, 0xA0, 0x3);
+
+  /*
+   * Connect the device driver handler that will be called when an
+   * interrupt for the device occurs. The handler performs the specific
+   * interrupt processing for the device.
+   */
+  XAxiDma_BdRing* txRingPtr = XAxiDma_GetTxRing(&mDev);
+
+  status = XScuGic_Connect(
+      &mIntCtrl, mIntCtrlId, (Xil_InterruptHandler)TxIRQCallback, this);
+  if (status != XST_SUCCESS) {
+    return status;
+  }
+
+  XScuGic_Enable(&mIntCtrl, mIntCtrlId);
+
+  /* Enable interrupts from the hardware */
+
+  Xil_ExceptionInit();
+  Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+                               (Xil_ExceptionHandler)XScuGic_InterruptHandler,
+                               (void*)&mIntCtrl);
+
+  Xil_ExceptionEnable();
+
+  return XST_SUCCESS;
+}
+
 bool AudioStreamDMA::Initialize() {
   cout << "Initialize DMA ..." << endl;
   int status = XAxiDma_CfgInitialize(&mDev, XAxiDma_LookupConfig(mDeviceId));
@@ -31,49 +210,18 @@ bool AudioStreamDMA::Initialize() {
     cout << "Device has Scatter-Gather engine mode" << endl;
   }
 
-  XAxiDma_BdRing* txRingPtr = XAxiDma_GetTxRing(&mDev);
-
   mDmaWritten = false;
 
-  // Disable all TX interrupts before TxBD space setup
-  XAxiDma_BdRingIntDisable(txRingPtr, XAXIDMA_IRQ_ALL_MASK);
-
-  // Setup TxBD space
-  uint32_t bd_count = XAxiDma_BdRingCntCalc(XAXIDMA_BD_MINIMUM_ALIGNMENT,
-                                            (uint32_t)sizeof(mBdBuffer));
-
-  status = XAxiDma_BdRingCreate(txRingPtr,
-                                (UINTPTR)&mBdBuffer[0],
-                                (UINTPTR)&mBdBuffer[0],
-                                XAXIDMA_BD_MINIMUM_ALIGNMENT,
-                                bd_count);
-  if (status != XST_SUCCESS) {
-    printf("Failed create BD ring\n");
+  bool ret = TxSetup();
+  if (!ret) {
+    LOG_ERROR("TxSetup failed");
     return false;
   }
 
-  // Like the RxBD space, we create a template and set all BDs to be the
-  // same as the template. The sender has to set up the BDs as needed.
-  XAxiDma_Bd BdTemplate;
-  XAxiDma_BdClear(&BdTemplate);
-  status = XAxiDma_BdRingClone(txRingPtr, &BdTemplate);
-  if (status != XST_SUCCESS) {
-    printf("Failed to clone BDs\n");
+  bool ret = InterruptSetup();
+  if (!ret) {
+    LOG_ERROR("InterruptSetup failed");
     return false;
-  }
-
-  // Enable Cyclic Mode
-  status = XAxiDma_SelectCyclicMode(&mDev, XAXIDMA_DEVICE_TO_DMA, 1);
-  if (status != XST_SUCCESS) {
-    printf("Failed to set cyclic mode\n");
-    return false;
-  }
-
-  // Start the TX channel
-  status = XAxiDma_BdRingStart(txRingPtr);
-  // status = XAxiDma_StartBdRingHw(txRingPtr);
-  if (status != XST_SUCCESS) {
-    printf("Failed bd start\n");
   }
 
   cout << "Done." << endl;
@@ -109,8 +257,8 @@ void AudioStreamDMA::TransmitBlob(DMABuffer const& buffer) {
 }
 
 /**
- * @brief Blocks can have a maximum size of "txRingPtr->MaxTransferLen" (around
- *        8 MBytes)
+ * @brief Blocks can have a maximum size of "txRingPtr->MaxTransferLen"
+ * (around 8 MBytes)
  */
 void AudioStreamDMA::Transmit(DMABuffer const& buffer, uint32_t n_repeats) {
   XAxiDma_BdRing* txRingPtr = XAxiDma_GetTxRing(&mDev);
--- hardware/vivado/sdk/fm_radio_app/src/AudioStreamDMA.h
+++ hardware/vivado/sdk/fm_radio_app/src/AudioStreamDMA.h
@@ -8,9 +8,13 @@
 #define _AUDIOSTREAMDMA_H_
 
 #include <xaxidma.h>
+#include <xscugic.h>
 
 // Size of the buffer which holds the DMA Buffer Descriptors (BDs)
-#define DMA_BUFFER_SIZE 400
+#define DMA_BD_BUFFER_SIZE        400  // NUMBER_OF_BDS_TO_TRANSFER
+#define DMA_COALESCING_COUNT      400
+#define DMA_DELAY_TIMER_COUNT     100
+#define DMA_RESET_TIMEOUT_COUNTER 10000
 
 typedef struct {
   uint8_t* buffer;
@@ -19,13 +23,23 @@ typedef struct {
 
 class AudioStreamDMA {
  private:
+  XScuGic mIntCtrl;
+  uint8_t const mIntCtrlId = XPAR_FABRIC_AXIDMA_0_VEC_ID;
+
   XAxiDma mDev;
-  XAxiDma_Bd mBdBuffer[DMA_BUFFER_SIZE]
+  XAxiDma_Bd mBdBuffer[DMA_BD_BUFFER_SIZE]
       __attribute__((aligned(XAXIDMA_BD_MINIMUM_ALIGNMENT)));
   uint8_t mDeviceId;
   bool mDmaWritten;
+  int mErrorState;
 
   void Transmit(DMABuffer const& buffer, uint32_t n_repeats);
+
+  bool TxSetup();
+  bool InterruptSetup();
+  static void TxIRQCallback(void* context);
+  void TxIRQHandler();
+  void TxDoneCallback();
 
  public:
   AudioStreamDMA(uint32_t device_id);
