c78a1db80230e2b09d489c6021fe9be85b8c4a7c
Michael Wurm <wurm.michael95@gmail.com>
2021-05-28 19:11:29 +0200

    scale output

--- sim/matlab/helpers/writeDataToFileWAV.m
+++ sim/matlab/helpers/writeDataToFileWAV.m
@@ -14,15 +14,23 @@ function status = writeDataToFileWAV(data, fs, filename)
 data_real = real(data);
 data_imag = imag(data);
 
+fprintf('before\n');
+fprintf('max(data_real): %f\n', max(data_real));
+fprintf('max(data_imag): %f\n', max(data_imag));
+
 % Scale data to int16
-data_real = int16(data_real*2^15);
-data_imag = int16(data_imag*2^15);
+data_real = int16(data_real*2^14);
+data_imag = int16(data_imag*2^14);
 
-fileAudioData = zeros(length(data),2);  % create empty data buffer for 2 channels
-fileAudioData(:,1) = data_real;         % real data in Left channel
-fileAudioData(:,2) = data_imag;         % imag data in Right channel
+fprintf('after\n');
+fprintf('max(data_real): %f\n', max(data_real));
+fprintf('max(data_imag): %f\n', max(data_imag));
 
-audiowrite(filename, fileAudioData, fs);
+fileAudioData = zeros(length(data),2,'int16');  % create empty data buffer for 2 channels
+fileAudioData(:,1) = data_real;                 % real data in Left channel
+fileAudioData(:,2) = data_imag;                 % imag data in Right channel
+
+audiowrite(filename, fileAudioData, fs,'BitsPerSample',16);
 
 status = true;
 end
