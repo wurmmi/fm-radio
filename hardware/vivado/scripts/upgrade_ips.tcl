784b5691638a4a8e2922f4341b9c31cf06ac61a2
Michael Wurm <wurm.michael95@gmail.com>
2021-06-20 16:21:43 +0200

    can't revert version nr..

--- hardware/vivado/scripts/upgrade_ips.tcl
+++ hardware/vivado/scripts/upgrade_ips.tcl
@@ -32,7 +32,6 @@ if {[file exists $build_dir/$proj_name.xpr]} {
   ###
   # NOTE:
   # This is a work-around, to re-import the VHDL files into the project ...
-  # ---> STEP 1
   ###
   puts "- Bump VHDL IP core_revision number"
   set comp [ ipx::open_core -set_current false [ get_property xml_file_name [ get_ipdefs *fm_receiver_vhdl* ] ] ]
@@ -46,16 +45,6 @@ if {[file exists $build_dir/$proj_name.xpr]} {
   puts "- Upgrade IP"
   upgrade_ip [get_ips]
   #upgrade_ip -vlnv MWURM:hls:fm_receiver_hls:0.1 [get_ips  proj_fm_receiver_hls_0_0] -log ip_upgrade.log
-
-  ###
-  # NOTE:
-  # ---> STEP 2
-  ###
-  puts "- Revert VHDL IP core_revision number"
-  set comp [ ipx::open_core -set_current false [ get_property xml_file_name [ get_ipdefs *fm_receiver_vhdl* ] ] ]
-  set_property core_revision [ expr [ get_property core_revision $comp ] - 1 ] $comp
-  ipx::save_core $comp
-  ipx::unload_core $comp
 
   puts "- Validate design"
   validate_bd_design
