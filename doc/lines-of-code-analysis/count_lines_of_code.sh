b588ea5762d477cfd4669f246bd202c104831557
Michael Wurm <wurm.michael95@gmail.com>
2021-06-29 23:49:58 +0200

    first 2 cloc commands

--- doc/lines-of-code-analysis/count_lines_of_code.sh
+++ doc/lines-of-code-analysis/count_lines_of_code.sh
@@ -1,7 +1,16 @@
+#!/usr/bin/env bash
 #-------------------------------------------------------------------------------
 # File        : count_lines_of_code.sh
 # Author      : Michael Wurm <wurm.michael95@gmail.com>
 # Description : Count lines of code in this repository and produce a statistic.
 #-------------------------------------------------------------------------------
 
-cloc hardware/vhdl/ip/rtl/ --by-file --not-match-f="fixed_"
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+cd $SCRIPT_PATH/../..
+echo "SCRIPT_PATH: $SCRIPT_PATH"
+
+# VHDL IP
+cloc hardware/vhdl/ip/rtl/ --by-file --not-match-f="fixed_" | tee $SCRIPT_PATH/VHDL_IP.txt
+
+# HLS IP
+cloc hardware/hls/ --by-file --not-match-f="fixed_" | tee $SCRIPT_PATH/HLS_IP.txt
