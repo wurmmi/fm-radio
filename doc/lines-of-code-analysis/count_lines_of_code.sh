494fb0f10dd27f07bb369cb117c67334c6df2601
Michael Wurm <wurm.michael95@gmail.com>
2021-06-29 23:51:21 +0200

    comments

--- doc/lines-of-code-analysis/count_lines_of_code.sh
+++ doc/lines-of-code-analysis/count_lines_of_code.sh
@@ -2,12 +2,18 @@
 #-------------------------------------------------------------------------------
 # File        : count_lines_of_code.sh
 # Author      : Michael Wurm <wurm.michael95@gmail.com>
-# Description : Count lines of code in this repository and produce a statistic.
+# Description : Count lines of code in this repository and produce some statistics.
 #-------------------------------------------------------------------------------
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 cd $SCRIPT_PATH/../..
 echo "SCRIPT_PATH: $SCRIPT_PATH"
+
+###
+# For more information on how to use CLOC, please visit:
+#   https://github.com/AlDanial/cloc
+###
+
 
 # VHDL IP
 cloc hardware/vhdl/ip/rtl/ --by-file --not-match-f="fixed_" | tee $SCRIPT_PATH/VHDL_IP.txt
