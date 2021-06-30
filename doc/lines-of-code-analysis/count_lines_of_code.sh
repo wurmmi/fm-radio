a2f3aa24abfe4a93867c377fa54f0edfdd12ff07
Michael Wurm <wurm.michael95@gmail.com>
2021-06-30 18:34:29 +0200

    vhdl tb and hls tb okay

--- doc/lines-of-code-analysis/count_lines_of_code.sh
+++ doc/lines-of-code-analysis/count_lines_of_code.sh
@@ -14,15 +14,15 @@ rm -f *.txt
 
 ###
 # NOTE:
-#  Analysis done with CLOC.
+#  Analysis is done with CLOC.
 #  For more information on how to use CLOC, please visit: https://github.com/AlDanial/cloc
 #
 #  Choose a cloc version with the CLOC_CMD.
-#  (1) need to 'sudo apt install cloc' (version 1.82 on Ubuntu 20.04)
-#  (2) need to have docker installed on your system - always uses latest available version
+#    (1) need to 'sudo apt install cloc' (version 1.82 on Ubuntu 20.04)
+#    (2) need to have Docker installed on your system - always uses latest available version
 ###
 #CLOC_CMD="cloc"
-CLOC_CMD="docker run --rm -v $REPO_ROOT:/tmp aldanial/cloc:latest"
+CLOC_CMD="docker run --rm -v $REPO_ROOT:/tmp/ aldanial/cloc:latest"
 
 cd $REPO_ROOT
 
@@ -42,14 +42,14 @@ $CLOC_CMD sim/matlab/                                 \
 #-------------------------------------------------------------------------------
 
 # VHDL IP
-$CLOC_CMD hardware/vhdl/                              \
+$CLOC_CMD hardware/vhdl/                         \
         --by-file-by-lang                        \
         --match-d='(rtl|utils)'                  \
         --not-match-f='(fixed_|fm_radio_axi|filter_(.*)_pkg)' \
             | tee $SCRIPT_PATH/hw_ip_design_vhdl.txt
 
 # HLS IP
-$CLOC_CMD hardware/hls/src/                           \
+$CLOC_CMD hardware/hls/src/                      \
         --by-file-by-lang                        \
         --not-match-f='(filter_(.*).h)'          \
             | tee $SCRIPT_PATH/hw_ip_design_hls.txt
@@ -59,25 +59,24 @@ $CLOC_CMD hardware/hls/src/                           \
 #-------------------------------------------------------------------------------
 
 # Common
-$CLOC_CMD hardware/common/                            \
+$CLOC_CMD hardware/common/                       \
         --by-file-by-lang                        \
-        --exclude-dir="fixed_point"        \
+        --exclude-dir="fixed_point"              \
             | tee $SCRIPT_PATH/hw_common.txt
 
-exit 0
-
 # VHDL testbench
-$CLOC_CMD hardware/vhdl/ip/tb/ \
-        --by-file \
-        --not-match-f="fixed_"       \
+$CLOC_CMD hardware/vhdl/ip/tb/                   \
+        --by-file-by-lang                        \
             | tee $SCRIPT_PATH/ip_testbench_vhdl.txt
 
 # HLS testbench
-$CLOC_CMD hardware/hls/tb/ \
-        --by-file-by-lang \
+$CLOC_CMD hardware/hls/                          \
+        --by-file-by-lang                        \
+        --match-d='(tb|scripts)'                 \
+        --not-match-f='(export|synth)'           \
             | tee $SCRIPT_PATH/ip_testbench_hls.txt
-
 
 #-------------------------------------------------------------------------------
 # Vivado
 #-------------------------------------------------------------------------------
+
