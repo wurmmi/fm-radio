a40f33f794157b6469aa65640f0fd10eb447828c
Michael Wurm <wurm.michael95@gmail.com>
2021-08-13 18:02:03 +0200

    beautify plot

--- doc/thesis/img/matlab/impl_time_analysis.py
+++ doc/thesis/img/matlab/impl_time_analysis.py
@@ -10,7 +10,7 @@ plt.rcParams.update({
     "text.usetex": True,
     "font.family": "serif",
     "font.sans-serif": ["Helvetica"],
-    "font.size": 18,
+    "font.size": 16,
 })
 
 x = ['VHDL', 'HLS', 'Matlab']
@@ -21,11 +21,15 @@ x_pos = [i for i, _ in enumerate(x)]
 fig = plt.figure()
 fig.set_size_inches(6, 5)
 ax = fig.add_subplot(1, 1, 1)
+
 ax.set_yscale('log')
 
-plt.bar(x_pos, energy, width=0.5)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.bar(x_pos, energy, width=0.5, edgecolor='black')
 plt.xlabel("Implementation Variant")
-plt.ylabel("Samples per second")
+#plt.ylabel("Samples per second")
 plt.title("Processed Samples Per Second")
 
 plt.xticks(x_pos, x)
