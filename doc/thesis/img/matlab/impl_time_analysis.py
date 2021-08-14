b06f0b1a1540dfccfc6c155f99f64557f9dd8e03
Michael Wurm <wurm.michael95@gmail.com>
2021-08-14 11:56:56 +0200

    beautify plots (factor arrows)

--- doc/thesis/img/matlab/impl_time_analysis.py
+++ doc/thesis/img/matlab/impl_time_analysis.py
@@ -13,10 +13,10 @@ plt.rcParams.update({
     "font.size": 16,
 })
 
-x = ['VHDL', 'HLS', 'Matlab']
-energy = [18, 45000, 3100000]
+variant = ['VHDL', 'HLS', 'Matlab']
+samples_per_second = [18, 45000, 3100000]
 
-x_pos = [i for i, _ in enumerate(x)]
+x_pos = [i for i, _ in enumerate(variant)]
 
 fig = plt.figure()
 fig.set_size_inches(6, 5)
@@ -27,12 +27,32 @@ ax.set_yscale('log')
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-plt.bar(x_pos, energy, width=0.5, edgecolor='black')
+plt.bar(x_pos, samples_per_second, width=0.5, edgecolor='black')
 plt.xlabel("Implementation Variant")
 #plt.ylabel("Samples per second")
 plt.title("Processed Samples Per Second")
 
-plt.xticks(x_pos, x)
+plt.xticks(x_pos, variant)
+
+plt.annotate('x2500',
+             xy=(0.5, 10000),
+             xycoords='data',
+             xytext=(-70, -40),
+             textcoords='offset points',
+             fontsize=16,
+             horizontalalignment='left',
+             arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=-0.2')
+             )
+plt.annotate('x70',
+             xy=(1.6, 1000000),
+             xycoords='data',
+             xytext=(-70, -40),
+             textcoords='offset points',
+             fontsize=16,
+             horizontalalignment='left',
+             arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=-0.2')
+             )
+
 
 # show and save
 plt.tight_layout()
