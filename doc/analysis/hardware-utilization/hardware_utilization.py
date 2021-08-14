6081e5876662980c8060c645ae2dcd63aaf4e2fd
Michael Wurm <wurm.michael95@gmail.com>
2021-08-14 16:08:35 +0200

    re-structure and beautify plot

--- doc/analysis/hardware-utilization/hardware_utilization.py
+++ doc/analysis/hardware-utilization/hardware_utilization.py
@@ -15,22 +15,15 @@ plt.rcParams.update({
 })
 
 # Data
-labels = ['VHDL', 'HLS']
-hw_util_lut = [1203, 650]
-hw_util_ff = [1361, 494]
-hw_util_lutram = [176, 0]
-hw_util_srl = [0, 56]
-hw_util_ramb18 = [0, 7]
-hw_util_dsp48 = [8, 6]
+labels = ['LUT', 'FF', 'LUTRAM', 'SRL', 'RAMB18', 'DSP48']
+values_vhdl = [1203, 1361, 176, 0, 0, 8]
+values_hls = [650, 494, 0, 56, 7, 6]
 
-barwidth = 0.15
+barwidth = 0.4
 
 # the label locations
-x1 = np.arange(len(labels))
+x1 = np.arange(len(labels)) * 1.5
 x2 = [x + barwidth for x in x1]
-x3 = [x + barwidth for x in x2]
-x4 = [x + barwidth for x in x3]
-x5 = [x + barwidth for x in x4]
 
 # Plot
 fig, ax = plt.subplots()
@@ -38,28 +31,20 @@ fig.set_size_inches(8, 5)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-rects1 = ax.bar(x2, hw_util_lut, barwidth, label='LUT', edgecolor='black')
-rects2 = ax.bar(x1, hw_util_ff, barwidth, label='FF', edgecolor='black')
-rects3 = ax.bar(x3, hw_util_lutram, barwidth, label='LUTRAM', edgecolor='black')
-rects4 = ax.bar(x3, hw_util_srl, barwidth, label='SRL', edgecolor='black')
-rects5 = ax.bar(x4, hw_util_ramb18, barwidth, label='RAMB18', edgecolor='black')
-rects6 = ax.bar(x5, hw_util_dsp48, barwidth, label='DSP48', edgecolor='black')
+rects1 = ax.bar(x1, values_vhdl, barwidth, label='VHDL', color=plt.cm.Greens(0.6), edgecolor='black')
+rects2 = ax.bar(x2, values_hls, barwidth, label='HLS', color=plt.cm.Blues(0.6), edgecolor='black')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 # ax.set_ylabel('Scores')
 ax.set_title('Hardware Utilization')
-#ax.set_xticks([r + barwidth for r in range(len(labels))], labels)
-plt.xticks([r + 2 * barwidth for r in range(len(labels))], labels)
+plt.xticks([r + barwidth / 2 for r in x1], labels)
 ax.set_xticklabels(labels)
-ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+ax.legend(loc='best')
 #ax.legend(loc=(1.0, 0.2))
+#ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
 
 ax.bar_label(rects1, padding=3)
 ax.bar_label(rects2, padding=3)
-ax.bar_label(rects3, padding=3)
-ax.bar_label(rects4, padding=3)
-ax.bar_label(rects5, padding=3)
-ax.bar_label(rects6, padding=3)
 
 # show and save
 plt.tight_layout()
